ac3548729d9dbdf51e3e982d761123b208f92f9c
glen.smith <glen.smith@localhost>
2007-12-11 20:51:53 +0000

    Link tag link to right category

--- grails-app/taglib/TagCloudTagLib.groovy
+++ grails-app/taglib/TagCloudTagLib.groovy
@@ -29,7 +29,7 @@ class TagCloudTagLib {
 
         // step 4: output your tags
         tagToFreq.each {tag, freq ->
-            out << "<a href='$tag' class='tagCloudSize" + (freq / catSize).intValue() + "'>"
+            out << "<a href='/$blogId/tags/$tag' class='tagCloudSize" + (freq / catSize).intValue() + "'>"
             out << tag
             out << "</a> "
             log.debug "Tag class of $tag with size $freq is " +  (freq / catSize).intValue()
