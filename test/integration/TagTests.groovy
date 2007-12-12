8265ebafa26c27a8398a88589b3f313a2fd2c8d8
glen.smith <glen.smith@localhost>
2007-12-12 03:23:33 +0000

    Started work on more complete test case.

--- test/integration/TagTests.groovy
+++ test/integration/TagTests.groovy
@@ -1,6 +1,31 @@
-class TagTests extends GroovyTestCase
-{
-  public void testSomething ()
-  {
-  }
+class TagTests extends GroovyTestCase {
+
+    // simple test case for the tagcloud
+    public void testTagCloud() {
+
+        Blog blog = new Blog(title: 'JUnit Blog', blogid: "junit").save()
+        println blog.errors.dump()
+
+        // setup some mock tags...
+        def tags = []
+        1.upto(5) {i ->
+            tags << new Tag(name: "tag-${i}", blog: blog).save()
+        }
+
+        //  now create some entries, each with some tags
+        def entries = []
+        0.upto(100) {i ->
+            BlogEntry be = new BlogEntry(title: "entry-${i}", body: "body-${i}")
+            blog.addToBlogEntries(be).save()
+            tags[0..i % 5].each {tag ->
+                be.addToTags(tag).save()
+            }
+            entries << be
+        }
+        blog.save(flush: true)
+
+        // now test our tagcloud
+        new TagCloudTagLib().tagCloud(blogId: "junit");
+
+    }
 }
